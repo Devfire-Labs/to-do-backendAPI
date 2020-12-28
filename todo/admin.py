f72808a0657b610835fcc2ed5329bfabeaffd1d7
Luis Bonilla <blessedluismi@gmail.com>
2020-12-27 22:26:54 -0400

    Update admin.py

--- todo/admin.py
+++ todo/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from .models import Todo
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
-superuser = User.objects.create_superuser(
-        username='luismi',
-        email='luis.parent@unicaribe.edu.do',
-        password='1234',
-)
+# superuser = User.objects.create_superuser(
+#        username='luismi',
+#        email='luis.parent@unicaribe.edu.do',
+#        password='1234',
+#)
 
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed')
