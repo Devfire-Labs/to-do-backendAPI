3cf0f5a364fa84aa0683c8eae8d4ea388384b19d
Luis Bonilla <blessedluismi@gmail.com>
2020-12-27 20:44:20 -0400

    testing admin console

--- Procfile
+++ Procfile
@@ -1,4 +1,9 @@
 release: python manage.py makemigrations --no-input
 release: python manage.py migrate --no-input
+release: python manage.py createsuperuser
+release: admin
+release: luis.parent@unicaribe.edu.do
+release: bonilla1234!
+release: bonilla1234!
 
 web: gunicorn backend.wsgi
--- backend/settings.py
+++ backend/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ADMIN_ENABLED = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -31,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
