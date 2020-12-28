2b9cdb4a2969d4d89808a29297ad14073692d2fb
Luis Bonilla <blessedluismi@gmail.com>
2020-12-27 20:25:56 -0400

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -7,5 +7,5 @@ router = routers.DefaultRouter()                      # add this
 router.register(r'todos', views.TodoView, 'todo')     # add this
 
 urlpatterns = [
-    path('admin/', admin.site.urls),         path('api/', include(router.urls))                # add this
+    path('', admin.site.urls),         path('', include(router.urls))                # add this
 ]
