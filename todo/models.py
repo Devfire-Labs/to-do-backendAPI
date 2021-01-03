d0df94361367a14878cd98b79de662ab5a4c49c1
Luis Bonilla <blessedluismi@gmail.com>
2021-01-02 22:21:24 -0400

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Todo(models.Model):
   title = models.CharField(max_length=120)
-  description = models.TextField(required=False)
+  description = models.TextField(blank=True)
   completed = models.BooleanField(default=False)
 
   def _str_(self):
